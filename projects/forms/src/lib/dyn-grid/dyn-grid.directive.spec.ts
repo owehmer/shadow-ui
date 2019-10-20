ac400d4cb30d5fb5687dc22dec1bd00ac8efad2b
owehmer <oliver.wehmer@gmail.com>
2019-10-20 20:19:36 +0200

    rowspan

--- projects/forms/src/lib/dyn-grid/dyn-grid.directive.spec.ts
+++ projects/forms/src/lib/dyn-grid/dyn-grid.directive.spec.ts
@@ -2,7 +2,7 @@ import { DynGridDirective } from './dyn-grid.directive';
 
 describe('DynGridDirective', () => {
   it('should create an instance', () => {
-    const directive = new DynGridDirective();
+    const directive = new DynGridDirective(null);
     expect(directive).toBeTruthy();
   });
 });
