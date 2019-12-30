fdcd98685f81655ed10dfee694025f4f8c18cc31
BuildTools <unconfigured@null.spigotmc.org>
2019-12-30 19:59:16 +0100

    Deleted empty tests

--- projects/forms/src/lib/form-elements/date-time-picker/switchable-input.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SwitchableInputDirective } from './switchable-input.directive';
-
-describe('SwitchableInputDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SwitchableInputDirective();
-    expect(directive).toBeTruthy();
-  });
-});
