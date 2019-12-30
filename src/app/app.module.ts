080ac80f3828512d94af0a16a580bc3220cb8092
BuildTools <unconfigured@null.spigotmc.org>
2019-12-30 19:39:50 +0100

    Fixed some playground-app stylings + imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { MyContentComponent } from './dialog-demo/my-content/my-content.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
