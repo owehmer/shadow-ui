6105402b6694c0248938500f88a8f314460caf0e
owehmer <45573843+owehmer@users.noreply.github.com>
2021-03-19 22:26:08 +0100

    Revert "Moved stuff i guess? (#23)"
    This reverts commit 3d873db6

---
+++ projects/utils/src/lib/dyn-grid/dyn-grid.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SdwDynGridDirective } from './dyn-grid.directive';
+
+describe('DynGridDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SdwDynGridDirective(null);
+    expect(directive).toBeTruthy();
+  });
+});
